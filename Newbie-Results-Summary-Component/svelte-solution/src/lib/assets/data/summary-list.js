e00bc401106f99234c8b3a70a3feb3264f276eb1
Tolga Topkaç <72669663+tolgatopkac@users.noreply.github.com>
2023-07-22 23:19:16 +0300

    fix text bg colors

--- Newbie-Results-Summary-Component/svelte-solution/src/lib/assets/data/summary-list.js
+++ Newbie-Results-Summary-Component/svelte-solution/src/lib/assets/data/summary-list.js
@@ -3,12 +3,16 @@ import { IconMemory, IconReaction, IconVerbal, IconVisual } from '$lib/assets/im
 export let summaryList = [
 	{
 		icon: IconReaction,
+		bgcolor: 'bg-red-50',
+		textcolor: 'text-red-500',
 		title: 'Reaction',
 		score: '80',
 		total: '100'
 	},
 	{
 		icon: IconMemory,
+		bgcolor: 'bg-orange-50',
+		textcolor: 'text-amber-400',
 		title: 'Memory',
 		score: '92',
 		total: '100'
@@ -16,12 +20,16 @@ export let summaryList = [
 	{
 		icon: IconVerbal,
 		title: 'Verbal',
+		bgcolor: 'bg-teal-50',
+		textcolor: 'text-emerald-500',
 		score: '61',
 		total: '100'
 	},
 	{
 		icon: IconVisual,
 		title: 'Visual',
+		bgcolor: 'bg-violet-50',
+		textcolor: 'text-indigo-700',
 		score: '73',
 		total: '100'
 	}
--- Newbie-Results-Summary-Component/svelte-solution/src/lib/components/SummaryList.svelte
+++ Newbie-Results-Summary-Component/svelte-solution/src/lib/components/SummaryList.svelte
@@ -5,13 +5,13 @@
 </script>
 
 {#each data as item}
-	<div class="flex justify-between p-4 bg-red-50 rounded-xl">
+	<div class="flex justify-between p-4 {item.bgcolor} rounded-xl">
 		<div class="flex gap-3">
 			<img src={item.icon} alt="" />
-			<p class="text-red-500">Reaction</p>
+			<p class={item.textcolor}>{item.title}</p>
 		</div>
 		<p class="text-coolGray400">
-			<span class="text-blueGray700">90</span> / <span>100</span>
+			<span class="text-blueGray700">{item.score}</span> / <span>100</span>
 		</p>
 	</div>
 {/each}
